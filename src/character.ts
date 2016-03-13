e1b34e8e1a82473745e9c598ee2b96af15ffaf8b
nojimarian <nojimarian@gmail.com>
2016-03-13 10:15:43 +0900

    homeとキャラクター画面だけとりあえずDBと接続

--- src/character.ts
+++ src/character.ts
@@ -1,9 +1,26 @@
 //import {computedFrom} from 'aurelia-framework';
 
-export class Character {
-  heading = 'ステータス';
-  level = 1;
-  favorability = 0;
-  discription = 'hogehoge';
-}
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client' ;
+import 'fetch';
+import {Router} from 'aurelia-router';
 
+export class Character {
+  heading = 'キャラクター一覧';
+  router;
+  static inject() { return [Router]; }
+  constructor(router) { this.router = router; }
+
+  response;
+  activate() {
+
+    var a: any = new HttpClient()
+        .fetch('http://54.238.223.36:3000/character')
+        .then(response => response.json())
+        .then(response => {
+          this.response = response;
+          console.log(this.response)
+        });
+    return a;
+  }
+}
--- src/home.ts
+++ src/home.ts
@@ -1,9 +1,23 @@
-//import {computedFrom} from 'aurelia-framework';
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client' ;
+import 'fetch';
+import {Router} from 'aurelia-router';
 
 export class Home {
-  heading = '今日は何をする？';
-  level = 1
-  current = 12000;
-  goal = 30000;
+  router;
+  static inject() { return [Router]; }
+  constructor(router) { this.router = router; }
+
+  response;
+  activate() {
+
+    var a: any = new HttpClient()
+        .fetch('http://54.238.223.36:3000/home')
+        .then(response => response.json())
+        .then(response => {
+          this.response = response;
+        });
+    return a;
+  }
 }
 
